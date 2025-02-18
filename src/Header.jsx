5341699b8e44934fce5566711f92231cb954eeb8
Venkat varma <kvenkatvarma@gmail.com>
2025-02-18 18:21:16 +0530

    routeopedia

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from "./images/react.png";
+function Header() {
+  return (
+    <div>
+        <img src={logo} alt='' style={{height :"35px", verticalAlign : "top"}}/>    
+        Header
+    </div>
+  )
+}
+
+export default Header
