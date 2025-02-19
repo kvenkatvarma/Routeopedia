ab44753d89cb5e21266666d9631a28feeec5fad8
Venkat varma <kvenkatvarma@gmail.com>
2025-02-19 16:36:23 +0530

    Navigate back

--- src/Pages/CreateProduct.jsx
+++ src/Pages/CreateProduct.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link, useNavigate,Navigate } from 'react-router-dom'
 
 function CreateProduct() {
+   const navigate = useNavigate();
+
   return (
     <div>
       Create Product
+      <button onClick={()=>{
+         navigate(-1)
+      }}>Go back</button>
     </div>
   )
 }
--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link, useNavigate,Navigate } from 'react-router-dom'
 
 function ProductList() {
+    const navigate = useNavigate();
   return (
     <div>
       Product List
+      <button onClick={()=>{
+         navigate("/product/create")
+      }}>Add Product</button>
     </div>
   )
 }
