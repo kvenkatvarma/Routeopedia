dcd4101b4df08c324d144fbef3b51f875d35e129
Venkat varma <kvenkatvarma@gmail.com>
2025-02-19 16:19:37 +0530

    useNavigate Hook

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 function Product() {
+  const navigate = useNavigate();
   return (
     <div>
       Product
+      <button onClick={()=>{
+         navigate("/product/create")
+      }}>Add Product</button>
     </div>
-  )
+  );
 }
 
 export default Product
