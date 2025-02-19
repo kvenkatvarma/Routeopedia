78f012bcfce55eaba9bbc42d70910975b777c124
Venkat varma <kvenkatvarma@gmail.com>
2025-02-19 16:29:11 +0530

    Another Navigate Method

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React ,{useState} from 'react'
+import { Link, useNavigate,Navigate } from 'react-router-dom'
 function Product() {
   const navigate = useNavigate();
+
+  const [goToProduct,setGoToProduct] = useState(()=>{
+      return false;
+  });
+
   return (
     <div>
       Product
@@ -9,8 +14,14 @@ function Product() {
          navigate("/product/create")
       }}>Add Product</button>
       <Link to="/product/details/5">
-      <button>Navigate to Product Detail - 5</button>
+        S<button>Navigate to Product Detail - 5</button>
       </Link>
+      {
+        goToProduct && <Navigate to ="/product/details/3"/>
+      }
+        <button onClick={()=>{
+        setGoToProduct({goToProduct : true})
+      }}>Navigate to Product -3 {useState}</button>
     </div>
   );
 }
